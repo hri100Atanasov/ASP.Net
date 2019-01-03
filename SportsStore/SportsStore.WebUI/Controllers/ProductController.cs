06b31f19fabd47ee1d7ef199fe3f8fe7bf0ffe65
Hristo Atanasov <hri100atanasov@gmail.com>
2019-01-03 18:00:16 +0200

    update

---
+++ SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using SportsStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRespository repository;
+
+        public ProductController(IProductRespository productRepository)
+        {
+            repository = productRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+    }
+}
